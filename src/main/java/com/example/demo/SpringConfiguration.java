ca0b77394001252765dbf867851186ddc3aec369
Diwakar Sharma <diwakar2034@yahoo.com>
2021-04-01 22:28:17 -0400

    initial commit

---
+++ src/main/java/com/example/demo/SpringConfiguration.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+               .authorizeRequests()
+
+                .antMatchers("/admin").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/user").access("hasRole('ROLE_USER')")
+
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}pass").roles("USER")
+                .and()
+                .withUser("dave").password("{noop}begreat").roles("ADMIN");
+
+    }
+}
